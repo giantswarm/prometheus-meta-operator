32bd96d3f9366171595acd9d29df5a1ac4a51927
Théo Brigitte <theo@giantswarm.io>
2019-12-17 12:18:35 +0100

    delete namespace (#10)

--- service/controller/resource/namespace/delete.go
+++ service/controller/resource/namespace/delete.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/giantswarm/microerror"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
@@ -13,8 +14,8 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
-	_, err = r.k8sClient.K8sClient().CoreV1().Namespaces().Create(namespace)
-	if apierrors.IsAlreadyExists(err) {
+	err = r.k8sClient.K8sClient().CoreV1().Namespaces().Delete(namespace.GetName(), &metav1.DeleteOptions{})
+	if apierrors.IsNotFound(err) {
 		// fall through
 	} else if err != nil {
 		return microerror.Mask(err)
