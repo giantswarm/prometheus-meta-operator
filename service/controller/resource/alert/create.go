29e65bfc0175e2cc8396b342783997a7b75e40ad
Theo Brigitte <theo.brigitte@gmail.com>
2019-12-20 10:35:36 +0100

    add alert package

---
+++ service/controller/resource/alert/create.go
@@ -0,0 +1,26 @@
+package servicemonitor
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+)
+
+func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	serviceMonitors, err := toServiceMonitors(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	for _, serviceMonitor := range serviceMonitors {
+		_, err = r.prometheusClient.MonitoringV1().ServiceMonitors(serviceMonitor.GetNamespace()).Create(serviceMonitor)
+		if apierrors.IsAlreadyExists(err) {
+			// fall through
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	return nil
+}
---
+++ service/controller/resource/alert/delete.go
@@ -0,0 +1,27 @@
+package servicemonitor
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	serviceMonitors, err := toServiceMonitors(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	for _, serviceMonitor := range serviceMonitors {
+		err := r.prometheusClient.MonitoringV1().ServiceMonitors(serviceMonitor.GetNamespace()).Delete(serviceMonitor.GetName(), &metav1.DeleteOptions{})
+		if apierrors.IsNotFound(err) {
+			// fall through
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	return nil
+}
