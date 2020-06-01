c16ba5af2e6d92b1064e49e056e66dc5e7d813e7
Théo Brigitte <theo@giantswarm.io>
2020-06-01 14:39:03 +0200

    prepare 0.2.0-dev (#41)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -6,7 +6,7 @@ var (
 	gitSHA        = "n/a"
 	name          = "prometheus-meta-operator"
 	source        = "https://github.com/giantswarm/prometheus-meta-operator"
-	version       = "0.1.1"
+	version       = "0.2.0-dev"
 )
 
 func BundleVersion() string {
