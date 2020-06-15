d53a935d6c3f6e7a1a1e616981f6b2949541a1a3
Théo Brigitte <theo@giantswarm.io>
2020-06-15 10:27:37 +0200

    delete frontend resource (#53)

--- service/controller/resource/frontend/create.go
+++ service/controller/resource/frontend/create.go
@@ -4,23 +4,15 @@ import (
 	"context"
 
 	"github.com/giantswarm/microerror"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
 func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
-	frontend, err := toFrontend(obj)
+	// This resource is being removed because we do not need it currently.
+	// see: https://github.com/giantswarm/giantswarm/issues/11479
+	err := r.EnsureDeleted(ctx, obj)
 	if err != nil {
 		return microerror.Mask(err)
 	}
-
-	r.logger.LogCtx(ctx, "level", "debug", "message", "creating frontend")
-	_, err = r.k8sClient.K8sClient().AppsV1().Deployments(frontend.GetNamespace()).Create(frontend)
-	if apierrors.IsAlreadyExists(err) {
-		// fall through
-	} else if err != nil {
-		return microerror.Mask(err)
-	}
-	r.logger.LogCtx(ctx, "level", "debug", "message", "created frontend")
 
 	return nil
 }
