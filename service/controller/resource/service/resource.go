61ee6572247ca29f1d785850ad9738eb494c3512
Theo Brigitte <theo.brigitte@gmail.com>
2020-06-22 11:36:06 +0200

    carry over spec.ClusterIP

--- service/controller/resource/service/resource.go
+++ service/controller/resource/service/resource.go
@@ -7,6 +7,7 @@ import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/prometheus-meta-operator/service/controller/resource/generic"
@@ -33,6 +34,42 @@ func New(config Config) (*generic.Resource, error) {
 		c := config.K8sClient.K8sClient().CoreV1().Services(namespace)
 		return wrappedClient{client: c}
 	}
+	toService := func(v interface{}) (metav1.Object, error) {
+		cluster, err := key.ToCluster(v)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+		service := &corev1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "prometheus",
+				Namespace: key.Namespace(cluster),
+			},
+			Spec: corev1.ServiceSpec{
+				Ports: []corev1.ServicePort{
+					{
+						Name:     "prometheus",
+						Port:     int32(9091),
+						Protocol: "TCP",
+					},
+				},
+				Selector: map[string]string{
+					"app": "frontend",
+				},
+			},
+		}
+
+		current, err := clientFunc(cluster.GetNamespace()).Get("prometheus", metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			// fall through
+		} else if err != nil {
+			return nil, microerror.Mask(err)
+		} else {
+			c := current.(*corev1.Service)
+			service.Spec.ClusterIP = c.Spec.ClusterIP
+		}
+
+		return service, nil
+	}
 
 	c := generic.Config{
 		ClientFunc:     clientFunc,
@@ -47,34 +84,6 @@ func New(config Config) (*generic.Resource, error) {
 	}
 
 	return r, nil
-}
-
-func toService(v interface{}) (metav1.Object, error) {
-	cluster, err := key.ToCluster(v)
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-
-	service := &corev1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "prometheus",
-			Namespace: key.Namespace(cluster),
-		},
-		Spec: corev1.ServiceSpec{
-			Ports: []corev1.ServicePort{
-				{
-					Name:     "prometheus",
-					Port:     int32(9091),
-					Protocol: "TCP",
-				},
-			},
-			Selector: map[string]string{
-				"app": "frontend",
-			},
-		},
-	}
-
-	return service, nil
 }
 
 func hasChanged(current, desired metav1.Object) bool {
