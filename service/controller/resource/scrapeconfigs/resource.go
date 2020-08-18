dc8b6226202a6c820b92e33cb33fff18c8e187fd
Theo Brigitte <theo.brigitte@gmail.com>
2020-08-18 12:34:17 +0200

    sprintf %d

--- service/controller/resource/scrapeconfigs/resource.go
+++ service/controller/resource/scrapeconfigs/resource.go
@@ -101,7 +101,7 @@ func getTemplateData(cluster metav1.Object, provider string, clients k8sclient.I
 
 		etcd = fmt.Sprintf("etcd.%s.k8s.%s:2379", key.ClusterID(cluster), infra.Spec.Cluster.DNS.Domain)
 	case *v1alpha1.AWSConfig:
-		etcd = fmt.Sprintf("%s:2379", v.Spec.Cluster.Etcd.Domain, v.Spec.Cluster.Etcd.Port)
+		etcd = fmt.Sprintf("%s:%d", v.Spec.Cluster.Etcd.Domain, v.Spec.Cluster.Etcd.Port)
 	default:
 		return nil, microerror.Maskf(wrongTypeError, fmt.Sprintf("%T", cluster))
 	}
