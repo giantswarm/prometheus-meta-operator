343d8839d28e4fab65fedb9e0669010a8607429c
Theo Brigitte <theo.brigitte@gmail.com>
2020-08-19 17:52:59 +0200

    use apiextensions@v2.1.0 everywhere

--- service/controller/resource/scrapeconfigs/resource.go
+++ service/controller/resource/scrapeconfigs/resource.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"reflect"
 
-	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/apiextensions/v2/pkg/apis/provider/v1alpha1"
 	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
