30b4d5ae830a28abb1bcbe19974b0608fdfb974f
Theo Brigitte <theo.brigitte@gmail.com>
2020-08-19 17:08:18 +0200

    add podmonitorselector

--- service/controller/resource/prometheus/resource.go
+++ service/controller/resource/prometheus/resource.go
@@ -124,6 +124,11 @@ func toPrometheus(v interface{}, createPVC bool, storageSize resource.Quantity) 
 			Secrets: []string{
 				key.Secret(),
 			},
+			PodMonitorSelector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					key.ClusterIDKey(): key.ClusterID(cluster),
+				},
+			},
 			ServiceMonitorSelector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{
 					key.ClusterIDKey(): key.ClusterID(cluster),
