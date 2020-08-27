cbb450b1743f06957c63d8e896a0af46688bc5c3
GitHub Action <action@github.com>
2020-08-27 15:12:37 +0000

    pkg/project: bump version to 1.1.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -6,7 +6,7 @@ var (
 	gitSHA        = "n/a"
 	name          = "prometheus-meta-operator"
 	source        = "https://github.com/giantswarm/prometheus-meta-operator"
-	version       = "1.1.0"
+	version       = "1.1.1-dev"
 )
 
 func BundleVersion() string {
