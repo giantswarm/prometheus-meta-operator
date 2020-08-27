7b70d59d5b43dcd9ddbc59eb33b96d38001622fb
Theo Brigitte <theo.brigitte@gmail.com>
2020-08-27 11:56:56 +0200

    wrap test in unittest.Runner

--- pkg/unittest/unittest.go
+++ pkg/unittest/unittest.go
@@ -1,6 +1,20 @@
 package unittest
 
-import "unicode"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"unicode"
+
+	"github.com/giantswarm/apiextensions/v2/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/microerror"
+	pkgruntime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"sigs.k8s.io/cluster-api/api/v1alpha2"
+)
 
 // NormalizeFileName converts all non-digit, non-letter runes in input string to
 // dash ('-'). Coalesces multiple dashes into one.
@@ -17,4 +31,132 @@ func NormalizeFileName(s string) string {
 		}
 	}
 	return string(result)
+}
+
+type Runner struct {
+	OutputDir string
+
+	inputDir string
+	files    []os.FileInfo
+	current  int
+	err      error
+}
+
+type Value struct {
+	Name   string
+	Input  pkgruntime.Object
+	Output []byte
+}
+
+func NewRunner(outputDir string) (*Runner, error) {
+	_, filename, _, ok := runtime.Caller(0)
+	fmt.Println(path.Dir(filename), ok)
+	inputDir, err := filepath.Abs(filepath.Join(path.Dir(filename), "input"))
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	files, err := ioutil.ReadDir(inputDir)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r := &Runner{
+		OutputDir: outputDir,
+		inputDir:  inputDir,
+		files:     files,
+		current:   -1,
+		err:       nil,
+	}
+
+	return r, nil
+}
+
+func (r *Runner) Next() bool {
+	if r.err != nil {
+		return false
+	}
+
+	r.current++
+	if len(r.files) <= r.current {
+		return false
+	}
+
+	return true
+}
+
+func (r *Runner) Value() Value {
+	input, err := r.inputValue()
+	if err != nil {
+		r.err = microerror.Mask(err)
+		return Value{}
+	}
+
+	output, err := r.outputValue()
+	if err != nil {
+		r.err = microerror.Mask(err)
+		return Value{}
+	}
+
+	v := Value{
+		Name:   r.files[r.current].Name(),
+		Input:  input,
+		Output: output,
+	}
+
+	return v
+}
+
+func InputFiles() ([]os.FileInfo, string, error) {
+	root, err := filepath.Abs("./input")
+	if err != nil {
+		return nil, "", microerror.Mask(err)
+	}
+
+	files, err := ioutil.ReadDir(root)
+	if err != nil {
+		return nil, "", microerror.Mask(err)
+	}
+
+	return files, root, nil
+}
+
+func (r *Runner) inputValue() (pkgruntime.Object, error) {
+	inputFile := filepath.Join(r.inputDir, r.files[r.current].Name())
+	inputData, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	scheme := pkgruntime.NewScheme()
+	err = v1alpha2.AddToScheme(scheme)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	err = v1alpha1.AddToScheme(scheme)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	codecs := serializer.NewCodecFactory(scheme)
+	deserializer := codecs.UniversalDeserializer()
+	input, err := pkgruntime.Decode(deserializer, inputData)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return input, nil
+}
+
+func (r *Runner) outputValue() ([]byte, error) {
+	outputFile := filepath.Join(r.OutputDir, r.files[r.current].Name())
+	output, err := ioutil.ReadFile(outputFile)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return output, nil
+}
+
+func (r *Runner) Err() error {
+	return r.err
 }
--- service/controller/resource/namespace/resource_test.go
+++ service/controller/resource/namespace/resource_test.go
@@ -3,86 +3,46 @@ package namespace
 import (
 	"bytes"
 	"flag"
-	"io/ioutil"
-	"log"
 	"path/filepath"
 	"testing"
 
 	"github.com/ghodss/yaml"
-	"github.com/giantswarm/apiextensions/v2/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/prometheus-meta-operator/pkg/unittest"
 	"github.com/google/go-cmp/cmp"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"sigs.k8s.io/cluster-api/api/v1alpha2"
 )
 
 var update = flag.Bool("update", false, "update .golden CF template file")
 
 func TestNamespace(t *testing.T) {
-	root := "../../../../pkg/unittest/input"
-	files, err := ioutil.ReadDir(root)
+	outputDir, err := filepath.Abs("./output")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 
-	for _, file := range files {
-		t.Run(file.Name(), func(t *testing.T) {
-			var input runtime.Object
-			{
-				inputFile := filepath.Join(root, file.Name())
-				inputData, err := ioutil.ReadFile(inputFile)
-				if err != nil {
-					t.Fatal(err)
-				}
+	runner, err := unittest.NewRunner(outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-				scheme := runtime.NewScheme()
-				err = v1alpha2.AddToScheme(scheme)
-				if err != nil {
-					t.Fatal(err)
-				}
-				err = v1alpha1.AddToScheme(scheme)
-				if err != nil {
-					t.Fatal(err)
-				}
-				codecs := serializer.NewCodecFactory(scheme)
-				deserializer := codecs.UniversalDeserializer()
-				input, err = runtime.Decode(deserializer, inputData)
-				if err != nil {
-					t.Fatal(err)
-				}
-			}
-
-			namespace, err := toNamespace(input)
+	for runner.Next() {
+		tc := runner.Value()
+		t.Run(tc.Name, func(t *testing.T) {
+			namespace, err := toNamespace(tc.Input)
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			var testResult []byte
-			{
-				testResult, err = yaml.Marshal(namespace)
-				if err != nil {
-					t.Fatal(err)
-				}
+			testResult, err := yaml.Marshal(namespace)
+			if err != nil {
+				t.Fatal(err)
 			}
 
-			var expectedOutput []byte
-			{
-				outputFile := filepath.Join("output", file.Name())
-				if *update {
-					err := ioutil.WriteFile(outputFile, testResult, 0644)
-					if err != nil {
-						t.Fatal(err)
-					}
-				}
-				expectedOutput, err = ioutil.ReadFile(outputFile)
-				if err != nil {
-					t.Fatal(err)
-				}
-			}
-
-			if !bytes.Equal(testResult, expectedOutput) {
-				t.Fatalf("\n\n%s\n", cmp.Diff(string(expectedOutput), string(testResult)))
+			if !bytes.Equal(testResult, tc.Output) {
+				t.Fatalf("\n\n%s\n", cmp.Diff(string(tc.Output), string(testResult)))
 			}
 		})
+	}
+	if err := runner.Err(); err != nil {
+		t.Fatal(err)
 	}
 }
