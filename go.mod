c882a32f4076936baecd782d06f2d17c08a4ad76
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-08-28 10:31:01 +0300

    Select Cluster type version based on CRD's storage version
    v1alpha2.Cluster and v1alpha3.Cluster are incompatible and when given storage
    version is manipulated with non-matching type, it will always result in data
    loss. Select used version during runtime based on CRD's storage version
    configuration.

--- go.mod
+++ go.mod
@@ -15,7 +15,9 @@ require (
 	github.com/spf13/viper v1.6.2
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
 	k8s.io/api v0.18.5
+	k8s.io/apiextensions-apiserver v0.18.5
 	k8s.io/apimachinery v0.18.5
 	k8s.io/client-go v0.18.5
 	sigs.k8s.io/cluster-api v0.3.7
+	sigs.k8s.io/controller-runtime v0.6.1
 )
--- service/controller/clusterapi/controller.go
+++ service/controller/clusterapi/controller.go
@@ -1,14 +1,19 @@
 package clusterapi
 
 import (
+	"context"
+
 	promclient "github.com/coreos/prometheus-operator/pkg/client/versioned"
 	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/v2/pkg/controller"
 	"github.com/giantswarm/operatorkit/v2/pkg/resource"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/cluster-api/api/v1alpha2"
+	capiv1alpha2 "sigs.k8s.io/cluster-api/api/v1alpha2"
+	capiv1alpha3 "sigs.k8s.io/cluster-api/api/v1alpha3"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/giantswarm/prometheus-meta-operator/pkg/project"
 	controllerresource "github.com/giantswarm/prometheus-meta-operator/service/controller/resource"
@@ -41,16 +46,19 @@ func NewController(config ControllerConfig) (*Controller, error) {
 		}
 	}
 
+	runtimeObjectFactoryFunc, err := getClusterFactoryFunc(config.K8sClient.CtrlClient())
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
 	var operatorkitController *controller.Controller
 	{
 		c := controller.Config{
-			K8sClient: config.K8sClient,
-			Logger:    config.Logger,
-			Name:      project.Name() + "-cluster-api-controller",
-			NewRuntimeObjectFunc: func() runtime.Object {
-				return new(v1alpha2.Cluster)
-			},
-			Resources: resources,
+			K8sClient:            config.K8sClient,
+			Logger:               config.Logger,
+			Name:                 project.Name() + "-cluster-api-controller",
+			NewRuntimeObjectFunc: runtimeObjectFactoryFunc,
+			Resources:            resources,
 		}
 
 		operatorkitController, err = controller.New(c)
@@ -64,4 +72,36 @@ func NewController(config ControllerConfig) (*Controller, error) {
 	}
 
 	return c, nil
+}
+
+func getClusterFactoryFunc(ctrlClient client.Client) (func() runtime.Object, error) {
+	var clusterCRD apiextensionsv1.CustomResourceDefinition
+	err := ctrlClient.Get(context.Background(), client.ObjectKey{
+		Name: "clusters.cluster.x-k8s.io",
+	}, &clusterCRD)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	// Find out configured storage version.
+	var storageVersion string
+	for _, v := range clusterCRD.Spec.Versions {
+		if v.Storage {
+			storageVersion = v.Name
+			break
+		}
+	}
+
+	// Decide which object to construct based on storage version.
+	var fn func() runtime.Object
+	switch storageVersion {
+	case "v1alpha2":
+		fn = func() runtime.Object { return new(capiv1alpha2.Cluster) }
+	case "v1alpha3":
+		fn = func() runtime.Object { return new(capiv1alpha3.Cluster) }
+	default:
+		return nil, microerror.Maskf(unsupportedStorageVersionError, "implementation does not support storage version %q", storageVersion)
+	}
+
+	return fn, nil
 }
