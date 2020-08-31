ad09b81e6504771bb2dbb35c79e1f55c8c812fe4
Theo Brigitte <theo.brigitte@gmail.com>
2020-08-31 17:18:08 +0200

    add etcdcertificates resource

---
+++ service/controller/resource/etcd-certificates/resource.go
@@ -0,0 +1,116 @@
+package etcdcertificates
+
+import (
+	"context"
+	"io/ioutil"
+	"reflect"
+
+	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/prometheus-meta-operator/service/controller/resource/generic"
+	"github.com/giantswarm/prometheus-meta-operator/service/key"
+)
+
+const (
+	Name = "etcd-certificates"
+)
+
+type Config struct {
+	K8sClient k8sclient.Interface
+	Logger    micrologger.Logger
+}
+
+// secretCopier provides a `ToCR` method which copies data from the source
+// cluster secret CR
+type secretCopier struct {
+	clientFunc func(string) generic.Interface
+}
+
+func New(config Config) (*generic.Resource, error) {
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	clientFunc := func(namespace string) generic.Interface {
+		c := config.K8sClient.K8sClient().CoreV1().Secrets(namespace)
+		return wrappedClient{client: c}
+	}
+
+	sc := secretCopier{clientFunc: clientFunc}
+
+	c := generic.Config{
+		ClientFunc:     clientFunc,
+		Logger:         config.Logger,
+		Name:           Name,
+		ToCR:           sc.ToCR,
+		HasChangedFunc: hasChanged,
+	}
+	r, err := generic.New(c)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return r, nil
+}
+
+func (sc *secretCopier) ToCR(v interface{}) (metav1.Object, error) {
+	cluster, err := key.ToCluster(v)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	data, err := sc.getSource(context.TODO(), v)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      key.EtcdSecret(),
+			Namespace: key.Namespace(cluster),
+		},
+		StringData: data,
+	}
+
+	return secret, nil
+}
+
+// getSource returns the Secret to be copied, i.e. default/$CLUSTER_ID-prometheus
+func (sc *secretCopier) getSource(ctx context.Context, v interface{}) (map[string]string, error) {
+	ca, err := ioutil.ReadFile("/etcd-client-certs/ca.pem")
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	crt, err := ioutil.ReadFile("/etcd-client-certs/crt.pem")
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	key, err := ioutil.ReadFile("/etcd-client-certs/key.pem")
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	data := map[string]string{
+		"ca":  string(ca),
+		"crt": string(crt),
+		"key": string(key),
+	}
+
+	return data, nil
+}
+
+func hasChanged(current, desired metav1.Object) bool {
+	c := current.(*corev1.Secret)
+	d := desired.(*corev1.Secret)
+
+	return !reflect.DeepEqual(c.Data, d.Data)
+}
