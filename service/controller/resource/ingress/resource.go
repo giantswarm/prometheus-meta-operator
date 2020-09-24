99db026ff91ba72e98df332e46df82d8fbdbb95d
mig4 <42650719@auril.club>
2020-09-24 10:24:25 +0100

    Add ingress resource object
    Based on Theo's example in giantswarm/giantswarm#11524

--- service/controller/resource/ingress/resource.go
+++ service/controller/resource/ingress/resource.go
@@ -1,6 +1,7 @@
 package ingress
 
 import (
+	"fmt"
 	"reflect"
 
 	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
@@ -10,6 +11,7 @@ import (
 	"github.com/giantswarm/prometheus-meta-operator/service/key"
 	"k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 const (
@@ -40,12 +42,14 @@ func New(config Config) (*generic.Resource, error) {
 	}
 
 	c := generic.Config{
-		ClientFunc:       clientFunc,
-		Logger:           config.Logger,
-		Name:             Name,
-		GetObjectMeta:    getObjectMeta,
-		GetDesiredObject: toIngress,
-		HasChangedFunc:   hasChanged,
+		ClientFunc:    clientFunc,
+		Logger:        config.Logger,
+		Name:          Name,
+		GetObjectMeta: getObjectMeta,
+		GetDesiredObject: func(v interface{}) (metav1.Object, error) {
+			return toIngress(v, config.BaseDomain)
+		},
+		HasChangedFunc: hasChanged,
 	}
 	r, err := generic.New(c)
 	if err != nil {
@@ -64,11 +68,61 @@ func getObjectMeta(v interface{}) (metav1.ObjectMeta, error) {
 	return metav1.ObjectMeta{
 		Name:      cluster.GetName(),
 		Namespace: key.Namespace(cluster),
+		Labels: map[string]string{
+			"app": "prometheus",
+		},
+		Annotations: map[string]string{
+			"kubernetes.io/ingress.class":                        "nginx",
+			"kubernetes.io/tls-acme":                             "true",
+			"nginx.ingress.kubernetes.io/auth-signin":            "https://$host/oauth2/start?rd=$escaped_request_uri",
+			"nginx.ingress.kubernetes.io/auth-url":               "https://$host/oauth2/auth",
+			"nginx.ingress.kubernetes.io/whitelist-source-range": "185.102.95.187/32,95.179.153.65/32",
+		},
 	}, nil
 }
 
-func toIngress(v interface{}) (metav1.Object, error) {
-	return &v1beta1.Ingress{}, nil
+func toIngress(v interface{}, baseDomain string) (metav1.Object, error) {
+	objectMeta, err := getObjectMeta(v)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	cluster, err := key.ToCluster(v)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	path := fmt.Sprintf("/%s", cluster.GetName())
+
+	return &v1beta1.Ingress{
+		ObjectMeta: objectMeta,
+		Spec: v1beta1.IngressSpec{
+			TLS: []v1beta1.IngressTLS{
+				{
+					Hosts:      []string{baseDomain},
+					SecretName: "monitoring-prometheus", // TODO: is this new?
+				},
+			},
+			Rules: []v1beta1.IngressRule{
+				{
+					Host: baseDomain,
+					IngressRuleValue: v1beta1.IngressRuleValue{
+						HTTP: &v1beta1.HTTPIngressRuleValue{
+							Paths: []v1beta1.HTTPIngressPath{
+								{
+									Backend: v1beta1.IngressBackend{
+										ServiceName: "prometheus-operated",
+										ServicePort: intstr.FromInt(9090),
+									},
+									Path: path,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}, nil
 }
 
 func hasChanged(current, desired metav1.Object) bool {
