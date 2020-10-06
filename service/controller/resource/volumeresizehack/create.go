737fd0f01afc03c555edc525f3e65bebdafa8f86
Theo Brigitte <theo.brigitte@gmail.com>
2020-10-07 01:31:02 +0200

    simplify

--- service/controller/resource/volumeresizehack/create.go
+++ service/controller/resource/volumeresizehack/create.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"fmt"
 	"reflect"
-	"time"
 
 	"github.com/giantswarm/microerror"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/prometheus-meta-operator/service/key"
@@ -19,23 +17,9 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", "creating")
-
-	// 1. pause prometheus
-	// 2. delete pvc
-	// 3. scale down sts
-	// 4. wait for pvc gone
-	// 5. scale up sts
-	// 6. unpause prometheus
+	r.logger.LogCtx(ctx, "level", "debug", "message", "checking if pvc need to be re-created")
 
 	namespace := key.Namespace(cluster)
-
-	// Get prometheus
-	prometheusName := key.ClusterID(cluster)
-	_, err = r.prometheusClient.MonitoringV1().Prometheuses(namespace).Get(ctx, prometheusName, metav1.GetOptions{})
-	if err != nil {
-		return microerror.Mask(err)
-	}
 
 	// Get StS
 	stsName := fmt.Sprintf("prometheus-%s", cluster.GetName())
@@ -44,49 +28,35 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
-	noPVCnoReplicas := false
-	resize := false
+	if len(currentStS.Spec.VolumeClaimTemplates) <= 0 {
+		// No PVC template found in StS, nothing to resize. Skip this resource.
+		r.logger.LogCtx(ctx, "level", "debug", "message", "skipping, no pvc found in sts")
+		return nil
+	}
+
 	// Get PVC
-	// TODO find by name : prometheus-df3yn-db-prometheus-df3yn-0
 	index := 0
 	desiredPVC := currentStS.Spec.VolumeClaimTemplates[index]
 	pvcName := fmt.Sprintf("%s-%s-%d", desiredPVC.GetName(), currentStS.GetName(), index)
 	currentPVC, err := r.k8sClient.K8sClient().CoreV1().PersistentVolumeClaims(namespace).Get(ctx, pvcName, metav1.GetOptions{})
-	if apierrors.IsNotFound(err) {
-		r.logger.LogCtx(ctx, "level", "debug", "message", "PVC NOT FOUND")
-		if *currentStS.Spec.Replicas <= 0 {
-			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("StS SCALED DOWN replicas=%d", *currentStS.Spec.Replicas))
-			noPVCnoReplicas = true
-		} else {
-			// TODO: return nil, when pvc is not present but replicas is set to 1, need time or there's another issue. This resource is not responsible here.
-			return microerror.Mask(err)
-		}
-	} else {
+	if err != nil {
+		// Skip when we cannot get PVC.
+		r.logger.LogCtx(ctx, "level", "debug", "message", "skipping, cannot get pvc", "error", microerror.Mask(err))
+		return nil
+	}
+
+	if !reflect.DeepEqual(desiredPVC.Spec.Resources.Requests, currentPVC.Spec.Resources.Requests) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "pvc need to be re-created")
+
+		// delete pvc
+		err = r.k8sClient.K8sClient().CoreV1().PersistentVolumeClaims(namespace).Delete(ctx, pvcName, metav1.DeleteOptions{})
 		if err != nil {
 			return microerror.Mask(err)
 		}
-		if !reflect.DeepEqual(desiredPVC.Spec, currentPVC.Spec) {
-			r.logger.LogCtx(ctx, "level", "debug", "message", "PVC DO NOT MATCH")
-			resize = true
-		}
-	}
-
-	if noPVCnoReplicas || resize {
-		if !noPVCnoReplicas {
-			// delete pvc
-			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("DELETING PVC %s/%s", namespace, pvcName))
-			err = r.k8sClient.K8sClient().CoreV1().PersistentVolumeClaims(namespace).Delete(ctx, pvcName, metav1.DeleteOptions{})
-			if err != nil {
-				return microerror.Mask(err)
-			}
-			r.logger.LogCtx(ctx, "level", "debug", "message", "DELETED PVC")
-			time.Sleep(5 * time.Second)
-		}
+		r.logger.LogCtx(ctx, "level", "debug", "message", "pvc re-creating was triggered")
 	} else {
-		r.logger.LogCtx(ctx, "level", "debug", "message", "PVC MATCH")
+		r.logger.LogCtx(ctx, "level", "debug", "message", "pvc do not need to be re-created")
 	}
-
-	r.logger.LogCtx(ctx, "level", "debug", "message", "created")
 
 	return nil
 }
