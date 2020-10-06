cad524c85d5eb23557fe46752ee8318ba5bde293
Theo Brigitte <theo.brigitte@gmail.com>
2020-10-06 22:20:48 +0200

    no scaling

--- service/controller/resource/volumeresizehack/create.go
+++ service/controller/resource/volumeresizehack/create.go
@@ -6,7 +6,6 @@ import (
 	"reflect"
 	"time"
 
-	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/microerror"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -92,46 +91,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 			}
 			r.logger.LogCtx(ctx, "level", "debug", "message", "DELETED PVC")
 			time.Sleep(5 * time.Second)
-
-			// scale down
-			r.logger.LogCtx(ctx, "level", "debug", "message", "SCALING DOWN")
-			*currentStS.Spec.Replicas = 0
-			pausedStS, err := r.k8sClient.K8sClient().AppsV1().StatefulSets(namespace).Update(ctx, currentStS, metav1.UpdateOptions{})
-			if err != nil {
-				return microerror.Mask(err)
-			}
-			currentStS = pausedStS
-			r.logger.LogCtx(ctx, "level", "debug", "message", "SCALED DOWN")
-			time.Sleep(5 * time.Second)
-
-			// wait 30s for pvc gone
-			r.logger.LogCtx(ctx, "level", "debug", "message", "WAITING PVC")
-			o := func() error {
-				_, err := r.k8sClient.K8sClient().CoreV1().PersistentVolumeClaims(namespace).Get(ctx, pvcName, metav1.GetOptions{})
-				if apierrors.IsNotFound(err) {
-					return nil
-				}
-
-				return microerror.Mask(pvcExist)
-			}
-			b := backoff.NewMaxRetries(6, 5*time.Second)
-			err = backoff.Retry(o, b)
-			if err != nil {
-				return microerror.Mask(err)
-			}
-			r.logger.LogCtx(ctx, "level", "debug", "message", "WAITED PVC")
-			time.Sleep(5 * time.Second)
 		}
-
-		// scale back up
-		r.logger.LogCtx(ctx, "level", "debug", "message", "SCALING UP")
-		*currentStS.Spec.Replicas = 1
-		_, err = r.k8sClient.K8sClient().AppsV1().StatefulSets(namespace).Update(ctx, currentStS, metav1.UpdateOptions{})
-		if err != nil {
-			return microerror.Mask(err)
-		}
-		r.logger.LogCtx(ctx, "level", "debug", "message", "SCALED UP")
-		time.Sleep(5 * time.Second)
 
 		// unpause
 		r.logger.LogCtx(ctx, "level", "debug", "message", "UNPAUSING PROMETHEUS")
