b6b6800dd335f3726e8eb5f79c9040de76d0168c
Theo Brigitte <theo.brigitte@gmail.com>
2020-10-15 13:30:53 +0200

    comment

--- service/controller/resource/heartbeat/resource.go
+++ service/controller/resource/heartbeat/resource.go
@@ -87,6 +87,7 @@ func toHeartbeat(v interface{}, installation string) (*heartbeat.Heartbeat, erro
 }
 
 func hasChanged(current, desired heartbeat.Heartbeat) bool {
+	// Ignore those fields for comparison by setting them to the same value.
 	current.Enabled = true
 	desired.Enabled = true
 	current.Expired = true
