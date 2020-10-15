7b660e8577611369b14c85ed2931f1f44b41fa39
Theo Brigitte <theo.brigitte@gmail.com>
2020-10-15 16:33:14 +0200

    implements toReceiver/toRoute

--- service/controller/resource/heartbeat-routing/helpers.go
+++ service/controller/resource/heartbeat-routing/helpers.go
@@ -2,20 +2,57 @@ package heartbeatrouting
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/prometheus/alertmanager/config"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/microerror"
+
+	commoncfg "github.com/prometheus/common/config"
+	"github.com/prometheus/common/model"
+
+	"github.com/giantswarm/prometheus-meta-operator/service/key"
 )
 
 func toRoute(cluster metav1.Object, installation string) config.Route {
-	// TODO: implement me.
-	return config.Route{}
+	name := fmt.Sprintf("heartbeat_%s_%s", installation, key.ClusterID(cluster))
+
+	return config.Route{
+		Receiver: name,
+		Match: map[string]string{
+			"cluster":      key.ClusterID(cluster),
+			"installation": installation,
+			"type":         "heartbeat",
+		},
+		Continue:       false,
+		GroupInterval:  &model.Duration(1 * time.Second),
+		GroupWait:      &model.Duration(1 * time.Second),
+		RepeatInterval: &model.Duration(15 * time.Second),
+	}
 }
 
-func toReceiver(cluster metav1.Object, installation string) config.Receiver {
-	// TODO: implement me.
-	return config.Receiver{}
+func toReceiver(cluster metav1.Object, installation string, opsgenieKey string) config.Receiver {
+	name := fmt.Sprintf("heartbeat_%s_%s", installation, key.ClusterID(cluster))
+
+	return config.Receiver{
+		Name: name,
+		WebhookConfigs: []*config.WebhookConfigs{
+			&config.WebhookConfigs{
+				URL: nil,
+				HTTPConfig: &commoncfg.HTTPClientConfig{
+					BasicAuth: &commoncfg.BasicAuth{
+						Password: commoncfg.Secret(opsgenieKey),
+					},
+				},
+				NotifierConfig: config.NotifierConfig{
+					VSendResolved: false,
+				},
+			},
+		},
+	}
 }
 
 func contains(cfg config.Config, receiver config.Receiver, route config.Route) bool {
