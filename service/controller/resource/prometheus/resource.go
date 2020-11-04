9842381aad90e3d8a63595332e43a8b09a804ef8
Theo Brigitte <theo.brigitte@gmail.com>
2020-11-04 14:14:15 +0100

    evenly spread prometheus pods

--- service/controller/resource/prometheus/resource.go
+++ service/controller/resource/prometheus/resource.go
@@ -248,6 +248,18 @@ func toPrometheus(v interface{}, config Config) (metav1.Object, error) {
 					},
 				},
 			},
+			TopologySpreadConstraints: []v1.TopologySpreadConstraint{
+				v1.TopologySpreadConstraint{
+					MaxSkew:           1,
+					TopologyKey:       "kubernetes.io/os",
+					WhenUnsatisfiable: v1.ScheduleAnyway,
+					LabelSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app": "prometheus",
+						},
+					},
+				},
+			},
 		},
 	}
 
