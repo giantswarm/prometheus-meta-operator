2f6cbf73b1f8cfd04888022ded5fe77d3706e969
mig4 <42650719@auril.club>
2020-11-25 17:51:50 +0000

    Add prometheusautoscaler resource
    Enables `VerticalPodAutoscaler` to manage requests and limits on
    the Prometheus `StatefulSet` which helps with scheduling and prevents
    Prometheus from consuming all available resources on a node it's
    running on.
    
    Closes: https://github.com/giantswarm/giantswarm/issues/14558

---
+++ service/controller/resource/prometheusautoscaler/resource.go
@@ -0,0 +1,70 @@
+package prometheusautoscaler
+
+import (
+	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	autoscaling "k8s.io/api/autoscaling/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	vpa_types "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
+	vpa_clientset "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/client/clientset/versioned"
+
+	"github.com/giantswarm/prometheus-meta-operator/service/controller/resource/generic"
+)
+
+const (
+	Name = "prometheusautoscaler"
+)
+
+type Config struct {
+	K8sClient k8sclient.Interface
+	VpaClient vpa_clientset.Interface
+	Logger    micrologger.Logger
+}
+
+func New(config Config) (*generic.Resource, error) {
+	clientFunc := func(namespace string) generic.Interface {
+		c := config.VpaClient.AutoscalingV1().VerticalPodAutoscalers(namespace)
+		return wrappedClient{client: c}
+	}
+
+	c := generic.Config{
+		ClientFunc:       clientFunc,
+		Logger:           config.Logger,
+		Name:             Name,
+		GetObjectMeta:    getObjectMeta,
+		GetDesiredObject: getObject,
+	}
+	r, err := generic.New(c)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return r, nil
+}
+
+func getObjectMeta(v interface{}) (metav1.ObjectMeta, error) {
+	return metav1.ObjectMeta{
+		Name: "prometheus",
+	}, nil
+}
+
+func getObject(v interface{}) (metav1.Object, error) {
+	objectMeta, err := getObjectMeta(v)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	vpa := &vpa_types.VerticalPodAutoscaler{
+		ObjectMeta: objectMeta,
+		Spec: vpa_types.VerticalPodAutoscalerSpec{
+			TargetRef: &autoscaling.CrossVersionObjectReference{
+				Kind:       "StatefulSet",
+				Name:       "prometheus",
+				APIVersion: "apps/v1",
+			},
+		},
+	}
+
+	return vpa, nil
+}
