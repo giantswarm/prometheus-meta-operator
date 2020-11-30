9c12a71837b10b24f918f143b992a1cd817c85bf
mig4 <42650719@auril.club>
2020-11-30 11:06:13 +0000

    Be explicit about VPA configuration
    in case of changes upstream.

--- service/controller/resource/verticalpodautoscaler/resource.go
+++ service/controller/resource/verticalpodautoscaler/resource.go
@@ -69,9 +69,9 @@ func getObject(v interface{}) (metav1.Object, error) {
 		return nil, microerror.Mask(err)
 	}
 
-	// updateModeAuto := vpa_types.UpdateModeAuto
-	// containerScalingModeAuto := vpa_types.ContainerScalingModeAuto
-	// containerControlledValuesRequestsAndLimits := vpa_types.ContainerControlledValuesRequestsAndLimits
+	updateModeAuto := vpa_types.UpdateModeAuto
+	containerScalingModeAuto := vpa_types.ContainerScalingModeAuto
+	containerControlledValuesRequestsAndLimits := vpa_types.ContainerControlledValuesRequestsAndLimits
 	vpa := &vpa_types.VerticalPodAutoscaler{
 		ObjectMeta: objectMeta,
 		Spec: vpa_types.VerticalPodAutoscalerSpec{
@@ -80,15 +80,15 @@ func getObject(v interface{}) (metav1.Object, error) {
 				Name:       key.PrometheusSTSName(cluster),
 				APIVersion: "apps/v1",
 			},
-			// UpdatePolicy: &vpa_types.PodUpdatePolicy{
-			// 	UpdateMode: &updateModeAuto,
-			// },
+			UpdatePolicy: &vpa_types.PodUpdatePolicy{
+				UpdateMode: &updateModeAuto,
+			},
 			ResourcePolicy: &vpa_types.PodResourcePolicy{
 				ContainerPolicies: []vpa_types.ContainerResourcePolicy{
 					{
-						ContainerName: key.PrometheusContainerName(),
-						// Mode:             &containerScalingModeAuto,
-						// ControlledValues: &containerControlledValuesRequestsAndLimits,
+						ContainerName:    key.PrometheusContainerName(),
+						Mode:             &containerScalingModeAuto,
+						ControlledValues: &containerControlledValuesRequestsAndLimits,
 					},
 				},
 			},
