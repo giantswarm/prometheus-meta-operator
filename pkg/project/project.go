4f6b7699aab6d418315159c08107d854f57de63e
GitHub Action <action@github.com>
2020-12-02 11:18:09 +0000

    Bump version to 1.12.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "prometheus-meta-operator"
 	source      = "https://github.com/giantswarm/prometheus-meta-operator"
-	version     = "1.12.0"
+	version     = "1.12.1-dev"
 )
 
 func Description() string {
