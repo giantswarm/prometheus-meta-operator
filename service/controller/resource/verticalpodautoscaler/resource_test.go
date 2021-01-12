80685cb9f78cb3892cefe77351db53931df158ec
Theo Brigitte <theo.brigitte@gmail.com>
2021-01-12 11:00:02 +0100

    fix test

--- service/controller/resource/verticalpodautoscaler/resource_test.go
+++ service/controller/resource/verticalpodautoscaler/resource_test.go
@@ -8,7 +8,10 @@ import (
 
 	"github.com/giantswarm/micrologger"
 
-	"github.com/giantswarm/k8sclient/v5/pkg/k8sclient"
+	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/runtime"
 	vpa_clientsetfake "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/client/clientset/versioned/fake"
 
 	"github.com/giantswarm/prometheus-meta-operator/pkg/unittest"
@@ -31,13 +34,37 @@ func TestVerticalPodAutoScaler(t *testing.T) {
 			t.Fatal(err)
 		}
 	}
+
+	var node runtime.Object
+	{
+		node = &v1.Node{
+			Status: v1.NodeStatus{
+				Allocatable: v1.ResourceList{
+					v1.ResourceMemory: resource.MustParse("10"),
+				},
+			},
+		}
+	}
+
+	var k8sClient *k8sclient.Clients
+	{
+		c := k8sclient.ClientsConfig{
+			Logger:        logger,
+			SchemeBuilder: k8sclient.SchemeBuilder(v1.SchemeBuilder),
+		}
+		k8sClient, err = k8sclient.NewFakeClients(c, node)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
 	c := unittest.Config{
 		OutputDir: outputDir,
 		T:         t,
 		TestFunc: func(v interface{}) (interface{}, error) {
 			c := Config{
 				Logger:    logger,
-				K8sClient: k8sclient.NewFakeClients(),
+				K8sClient: k8sClient,
 				VpaClient: vpa_clientsetfake.NewSimpleClientset(),
 			}
 			r, err := New(c)
