5e6153de1d43f7e5df4616bff4c6348960362ecb
Theo Brigitte <theo.brigitte@gmail.com>
2021-09-07 12:39:29 +0200

    reconcile v1alpha3 cluster only, webhook does the conversion job for us

--- service/controller/clusterapi/controller.go
+++ service/controller/clusterapi/controller.go
@@ -1,20 +1,15 @@
 package clusterapi
 
 import (
-	"context"
-
 	"github.com/giantswarm/k8sclient/v5/pkg/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/v4/pkg/controller"
 	"github.com/giantswarm/operatorkit/v4/pkg/resource"
 	promclient "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	vpa_clientset "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/client/clientset/versioned"
 	capiv1alpha3 "sigs.k8s.io/cluster-api/api/v1alpha3"
-	capiv1alpha4 "sigs.k8s.io/cluster-api/api/v1alpha4"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/giantswarm/prometheus-meta-operator/pkg/project"
 	controllerresource "github.com/giantswarm/prometheus-meta-operator/service/controller/resource"
@@ -74,19 +69,16 @@ func NewController(config ControllerConfig) (*Controller, error) {
 		}
 	}
 
-	runtimeObjectFactoryFunc, err := getClusterFactoryFunc(config.K8sClient.CtrlClient())
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-
 	var operatorkitController *controller.Controller
 	{
 		c := controller.Config{
-			K8sClient:            config.K8sClient,
-			Logger:               config.Logger,
-			Name:                 project.Name() + "-cluster-api-controller",
-			NewRuntimeObjectFunc: runtimeObjectFactoryFunc,
-			Resources:            resources,
+			K8sClient: config.K8sClient,
+			Logger:    config.Logger,
+			Name:      project.Name() + "-cluster-api-controller",
+			NewRuntimeObjectFunc: func() runtime.Object {
+				return new(capiv1alpha3.Cluster)
+			},
+			Resources: resources,
 		}
 
 		operatorkitController, err = controller.New(c)
@@ -100,36 +92,4 @@ func NewController(config ControllerConfig) (*Controller, error) {
 	}
 
 	return c, nil
-}
-
-func getClusterFactoryFunc(ctrlClient client.Client) (func() runtime.Object, error) {
-	var clusterCRD apiextensionsv1.CustomResourceDefinition
-	err := ctrlClient.Get(context.Background(), client.ObjectKey{
-		Name: "clusters.cluster.x-k8s.io",
-	}, &clusterCRD)
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-
-	// Find out configured storage version.
-	var storageVersion string
-	for _, v := range clusterCRD.Spec.Versions {
-		if v.Storage {
-			storageVersion = v.Name
-			break
-		}
-	}
-
-	// Decide which object to construct based on storage version.
-	var fn func() runtime.Object
-	switch storageVersion {
-	case "v1alpha3":
-		fn = func() runtime.Object { return new(capiv1alpha3.Cluster) }
-	case "v1alpha4":
-		fn = func() runtime.Object { return new(capiv1alpha4.Cluster) }
-	default:
-		return nil, microerror.Maskf(unsupportedStorageVersionError, "implementation does not support storage version %q", storageVersion)
-	}
-
-	return fn, nil
 }
