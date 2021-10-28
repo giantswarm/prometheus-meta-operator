0d73577c99ae08d283d01a392926591e18e91f55
GitHub Action <action@github.com>
2021-10-28 13:39:47 +0000

    Bump version to 1.51.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "prometheus-meta-operator"
 	source      = "https://github.com/giantswarm/prometheus-meta-operator"
-	version     = "1.51.2"
+	version     = "1.51.3-dev"
 )
 
 func Description() string {
