42c39c5ac2695187c874fd08e364f1da7f212f24
Théo Brigitte <theo@giantswarm.io>
2021-12-21 10:01:53 +0100

    Upgrade dependencies (#772)
    * upgrade github.com/spf13/viper v1.9.0 to v1.10.0
    
    * upgrade to go 1.17
    
    * upgrade github.com/giantswarm/microendpoint v0.2.0 to v0.3.0
    
    * upgrade github.com/giantswarm/microerror v0.3.0 to v0.4.0
    
    * upgrade github.com/giantswarm/microkit v0.2.2 to v0.3.0
    
    * upgrade github.com/giantswarm/micrologger v0.5.0 to v0.6.0
    
    * remove versionbundle.Changelog
    
    * upgrade github.com/giantswarm/versionbundle v0.3.0 to v1.0.0
    
    * upgrade github.com/giantswarm/microkit v0.3.0 to v1.0.0

--- pkg/project/version_bundle.go
+++ pkg/project/version_bundle.go
@@ -6,13 +6,6 @@ import (
 
 func NewVersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
-		Changelogs: []versionbundle.Changelog{
-			{
-				Component:   "prometheus-meta-operator",
-				Description: "The prometheus-meta-operator manages Kubernetes clusters monitoring.",
-				Kind:        versionbundle.KindChanged,
-			},
-		},
 		Components: []versionbundle.Component{},
 		Name:       "prometheus-meta-operator",
 		Version:    Version(),
