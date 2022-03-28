eb207602f3b4f6db2c04255487c52616132e2229
GitHub Action <action@github.com>
2022-03-28 12:18:56 +0000

    Bump version to 3.0.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "prometheus-meta-operator"
 	source      = "https://github.com/giantswarm/prometheus-meta-operator"
-	version     = "3.0.0"
+	version     = "3.0.1-dev"
 )
 
 func Description() string {
