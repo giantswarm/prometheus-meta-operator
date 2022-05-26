0789b7daa08c67f8e3e0d8da0db4bc4967178c3e
Mohamed Chiheb Ben Jemaa <mohamed@giantswarm.io>
2022-05-26 15:58:44 +0100

    Add boilerplate remotewrite controller (#900)
    * Add boilerplate remotewrite controller
    
    * update Changelog
    
    * cleanup unused attributes
    
    * Remove remotewrite wiring
    
    Co-authored-by: Hervé Nicol <hervenicol@users.noreply.github.com>

---
+++ service/controller/remotewrite/controller.go
@@ -0,0 +1,47 @@
+package remotewrite
+
+import (
+	"github.com/giantswarm/k8sclient/v7/pkg/k8sclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/v7/pkg/controller"
+	promclient "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned"
+
+	"github.com/giantswarm/prometheus-meta-operator/pkg/project"
+)
+
+type ControllerConfig struct {
+	K8sClient        k8sclient.Interface
+	Logger           micrologger.Logger
+	PrometheusClient promclient.Interface
+}
+
+type Controller struct {
+	*controller.Controller
+}
+
+func NewController(config ControllerConfig) (*Controller, error) {
+	var err error
+
+	var operatorkitController *controller.Controller
+	{
+		c := controller.Config{
+			K8sClient: config.K8sClient,
+			Logger:    config.Logger,
+			Name:      project.Name() + "-remote-write-controller",
+
+			//Resources: resources,
+		}
+
+		operatorkitController, err = controller.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	c := &Controller{
+		Controller: operatorkitController,
+	}
+
+	return c, nil
+}
