197d8a5307b9aed8edae0d806cd68dde637c0be6
Quentin Bisson <quentin@giantswarm.io>
2022-08-02 16:32:05 +0200

    Fix psp names (#962)
    * Fix psp names
    
    * Move cluster role to templates

--- service/controller/resource/rbac/delete.go
+++ service/controller/resource/rbac/delete.go
@@ -11,20 +11,6 @@ import (
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 	r.logger.Debugf(ctx, "deleting")
 	{
-		desired, err := toClusterRole(obj)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-		err = r.k8sClient.K8sClient().RbacV1().ClusterRoles().Delete(ctx, desired.GetName(), metav1.DeleteOptions{})
-		if apierrors.IsNotFound(err) {
-			// fall through
-		} else if err != nil {
-			return microerror.Mask(err)
-		}
-	}
-
-	{
 		desired, err := toClusterRoleBinding(obj)
 		if err != nil {
 			return microerror.Mask(err)
