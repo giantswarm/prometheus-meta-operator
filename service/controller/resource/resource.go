e6189c41488eb7112f2604af5303e7c230446fef
Quentin Bisson <quentin@giantswarm.io>
2022-08-24 17:27:20 +0200

    Fix secret issues on CAPI (#981)

--- service/controller/resource/resource.go
+++ service/controller/resource/resource.go
@@ -95,10 +95,10 @@ func New(config Config) ([]resource.Interface, error) {
 				},
 				{
 					NameFunc:      key.CAPICertificateName,
-					NamespaceFunc: key.CAPICertificateNamespace,
+					NamespaceFunc: key.OrganizationNamespace,
 				},
 			},
-			Target: key.SecretAPICertificates,
+			Target: key.Secret,
 		}
 
 		apiCertificatesResource, err = certificates.New(c)
