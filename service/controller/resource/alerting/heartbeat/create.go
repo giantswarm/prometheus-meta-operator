e043c59caebead54e7efb79904389bb53f7b31d5
Quentin Bisson <quentin@giantswarm.io>
2023-02-02 09:40:30 +0100

    Fix heartbeat update mechanism (#1150)

--- service/controller/resource/alerting/heartbeat/create.go
+++ service/controller/resource/alerting/heartbeat/create.go
@@ -13,67 +13,54 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
-	r.logger.Debugf(ctx, "checking if heartbeat exists")
-	var current heartbeat.Heartbeat
-	getResult, err := r.heartbeatClient.Get(ctx, desired.Name)
+	// By default, we create the heartbeat unless is it already configured and does not need to be updated;
+	var heartbeatHasToBeCreated bool = true
 
-	if IsApiNotFoundError(err) {
-		r.logger.Debugf(ctx, "heartbeat does not exist")
+	r.logger.Debugf(ctx, "checking if heartbeat is already configured")
+	getResult, err := r.heartbeatClient.Get(ctx, desired.Name)
+	heartbeatAlreadyConfigured := !IsApiNotFoundError(err)
+
+	if heartbeatAlreadyConfigured && err != nil {
+		r.logger.Debugf(ctx, "heartbeat is configured but could not be fetched")
+		return microerror.Mask(err)
+	}
+
+	if heartbeatAlreadyConfigured {
+		// We need to delete and recreate it because the update is a PATCH (so existing alert tags are kept)
+		// This causes issue when installations are switched from the testing pipeline to the stable pipeline as heartbeat are skipped.
+		r.logger.Debugf(ctx, "heartbeat is configured")
+		r.logger.Debugf(ctx, "checking if heartbeat needs to be reconfigured")
+
+		var current heartbeat.Heartbeat = getResult.Heartbeat
+		if hasChanged(current, *desired) {
+			r.logger.Debugf(ctx, "heartbeat has changed and needs to be reconfigured")
+
+			r.logger.Debugf(ctx, "deleting heartbeat")
+			_, err := r.heartbeatClient.Delete(ctx, desired.Name)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			r.logger.Debugf(ctx, "deleted heartbeat")
+		} else {
+			heartbeatHasToBeCreated = false
+			r.logger.Debugf(ctx, "heartbeat is up to date")
+		}
+	}
+
+	if heartbeatHasToBeCreated {
 		r.logger.Debugf(ctx, "creating heartbeat")
-		err := r.createHeartbeat(ctx, desired)
+		err := r.createAndPingHeartbeat(ctx, desired)
 		if err != nil {
 			return microerror.Mask(err)
 		}
 		r.logger.Debugf(ctx, "created heartbeat")
-
-		// The initial ping to the heartbeat is there to move the heartbeat from inactive to active.
-		_, err = r.heartbeatClient.Ping(ctx, desired.Name)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-		return nil
-	} else if err != nil {
-		return microerror.Mask(err)
-	}
-
-	r.logger.Debugf(ctx, "heartbeat exists")
-	current = getResult.Heartbeat
-
-	// We get the ID back from opsgenie so we update it in the heartbeat
-	if desired.OwnerTeam.Name == current.OwnerTeam.Name {
-		desired.OwnerTeam = current.OwnerTeam
-	}
-
-	r.logger.Debugf(ctx, "checking if heartbeat needs update")
-
-	if hasChanged(current, *desired) {
-		r.logger.Debugf(ctx, "heartbeat needs update")
-		r.logger.Debugf(ctx, "updating heartbeat")
-		req := &heartbeat.UpdateRequest{
-			Name:          desired.Name,
-			Description:   desired.Description,
-			Interval:      desired.Interval,
-			IntervalUnit:  heartbeat.Unit(desired.IntervalUnit),
-			Enabled:       &desired.Enabled,
-			OwnerTeam:     desired.OwnerTeam,
-			AlertMessage:  desired.AlertMessage,
-			AlertTag:      desired.AlertTags,
-			AlertPriority: desired.AlertPriority,
-		}
-		_, err := r.heartbeatClient.Update(ctx, req)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-		r.logger.Debugf(ctx, "updated heartbeat")
-	} else {
-		r.logger.Debugf(ctx, "heartbeat is up to date")
 	}
 
 	return nil
 }
 
-func (r *Resource) createHeartbeat(ctx context.Context, h *heartbeat.Heartbeat) error {
+func (r *Resource) createAndPingHeartbeat(ctx context.Context, h *heartbeat.Heartbeat) error {
 	req := &heartbeat.AddRequest{
 		Name:          h.Name,
 		Description:   h.Description,
@@ -89,5 +76,12 @@ func (r *Resource) createHeartbeat(ctx context.Context, h *heartbeat.Heartbeat) 
 	if err != nil {
 		return microerror.Mask(err)
 	}
+
+	// The initial ping to the heartbeat is there to move the heartbeat from inactive to active.
+	_, err = r.heartbeatClient.Ping(ctx, h.Name)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
 	return nil
 }
