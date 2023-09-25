7227cadb353d63bf7adb30e787271ac21fc40ea3
QuentinBisson <quentin@giantswarm.io>
2023-09-25 12:11:06 +0200

    Support absolute Grafana dashboard urls
    Signed-off-by: QuentinBisson <quentin@giantswarm.io>

--- pkg/template/template.go
+++ pkg/template/template.go
@@ -15,7 +15,12 @@ import (
 func RenderTemplate(templateData interface{}, templateLocation string) ([]byte, error) {
 	tpl := template.New("_base").Delims("[[", "]]")
 
+	// Clone the func map because we are adding context-specific functions.
 	var funcMap template.FuncMap = map[string]interface{}{}
+	for k, v := range sprig.FuncMap() {
+		funcMap[k] = v
+	}
+
 	// copied from: https://github.com/helm/helm/blob/8648ccf5d35d682dcd5f7a9c2082f0aaf071e817/pkg/engine/engine.go#L147-L154
 	funcMap["include"] = func(name string, data interface{}) (string, error) {
 		buf := bytes.NewBuffer(nil)
@@ -25,7 +30,7 @@ func RenderTemplate(templateData interface{}, templateLocation string) ([]byte, 
 		return buf.String(), nil
 	}
 
-	tpl, err := tpl.Funcs(sprig.FuncMap()).Funcs(funcMap).ParseGlob(templateLocation)
+	tpl, err := tpl.Funcs(funcMap).ParseGlob(templateLocation)
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
