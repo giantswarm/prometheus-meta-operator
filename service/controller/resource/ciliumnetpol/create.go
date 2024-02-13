afceac9c0c4a26158e70bc42ca92c614f7f0a468
QuantumEnigmaa <thibaud@giantswarm.io>
2024-02-13 11:50:44 +0100

    add Namespace method to the get one

--- service/controller/resource/ciliumnetpol/create.go
+++ service/controller/resource/ciliumnetpol/create.go
@@ -23,7 +23,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 			return microerror.Mask(err)
 		}
 
-		ciliumnetpol, err := r.dynamicK8sClient.Resource(resource).Get(ctx, desired.GetName(), metav1.GetOptions{})
+		ciliumnetpol, err := r.dynamicK8sClient.Resource(resource).Namespace(desired.GetNamespace()).Get(ctx, desired.GetName(), metav1.GetOptions{})
 		r.logger.Debugf(ctx, "ciliumnetpol: %#v", ciliumnetpol)
 		if apierrors.IsNotFound(err) {
 			_, err = r.dynamicK8sClient.Resource(resource).Namespace(desired.GetNamespace()).Create(ctx, desired, metav1.CreateOptions{})
