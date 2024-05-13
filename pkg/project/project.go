43fd2a04bae39627e592fbd4e2a22589da127441
taylorbot <dev@giantswarm.io>
2024-05-13 07:45:42 +0000

    Bump version to 4.75.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "prometheus-meta-operator"
 	source      = "https://github.com/giantswarm/prometheus-meta-operator"
-	version     = "4.75.0"
+	version     = "4.75.1-dev"
 )
 
 func Description() string {
