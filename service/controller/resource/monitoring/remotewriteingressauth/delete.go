dcd30691fc45c9765be1d9d1117b73766a0e0831
Quentin Bisson <quentin@giantswarm.io>
2024-06-06 13:17:13 +0200

    remove-generic-resource-from-remotewriteingress-resource (#1646)
    Signed-off-by: QuentinBisson <quentin@giantswarm.io>

---
+++ service/controller/resource/monitoring/remotewriteingressauth/delete.go
@@ -0,0 +1,27 @@
+package remotewriteingressauth
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	object, err := r.getObjectMeta(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	r.config.Logger.Debugf(ctx, "deleting")
+	err = r.config.K8sClient.K8sClient().CoreV1().Secrets(object.GetNamespace()).Delete(ctx, object.GetName(), metav1.DeleteOptions{})
+	if apierrors.IsNotFound(err) {
+		// fall through
+	} else if err != nil {
+		return microerror.Mask(err)
+	}
+	r.config.Logger.Debugf(ctx, "deleted")
+
+	return nil
+}
