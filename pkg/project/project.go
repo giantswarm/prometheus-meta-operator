d6968689e779b4c9b83d326bd6c9b9401c2da9e6
taylorbot <dev@giantswarm.io>
2025-01-06 09:44:24 +0000

    Release v4.84.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "prometheus-meta-operator"
 	source      = "https://github.com/giantswarm/prometheus-meta-operator"
-	version     = "4.83.1-dev"
+	version     = "4.84.0"
 )
 
 func Description() string {
